d532dbee46ff50120f4f5af174934bd4d05c1777
Samuel Alves <60525968+samuelsnaker@users.noreply.github.com>
2020-03-03 18:21:25 -0300

    "Adicionando Index.html e main.js"

---
+++ Aula_Javascript/js/main.js
@@ -0,0 +1,11 @@
+
+var nome="Samuel Alves";
+var n1 = 18;
+var n2 = 10
+var frase= "Japão é o melhor time do mundo";
+//alert(nome + " tem" + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.replace("japão", "Brasil"));
+alert(frase.replace("Japão", "Brasil"));
