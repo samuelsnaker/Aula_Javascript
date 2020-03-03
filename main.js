3bec261990a8e990ecbf05591aa2efcf55955bb6
Samuel Alves <60525968+samuelsnaker@users.noreply.github.com>
2020-03-03 18:36:23 -0300

    Adicionando Arrays e Dicionário

---
+++ main.js
@@ -0,0 +1,27 @@
+
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+//var fruta={nome:"maçã", cor:"vermelha"}
+//console.log(fruta.nome);
+//alert(fruta.cor);
+
+//var lista =["maçã", "Pêra", "Laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista[0]);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+//var nome="Samuel Alves";
+//var n1 = 18;
+//var n2 = 10
+//var frase= "Japão é o melhor time do mundo";
+//alert(nome + " tem" + idade + " anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 + n2);
+//console.log(frase.replace("japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
