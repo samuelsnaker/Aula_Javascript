9dd0a6628d47916bb7698e87fb151dde8e6dc2cb
Samuel Alves <60525968+samuelsnaker@users.noreply.github.com>
2020-03-04 20:33:21 -0300

    Atribuindo Buttons e Functions de interações

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
-function soma(n1,n2){
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+
+function load(){
+    alert("Página carregada");
+}
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+   //console.log(document.getElementById("Agradecimento"));
+  // alert("Obrigado por Clicar");
+}
+
+function redirecionar(){
+    window.open("https://www.google.com.br/");
+   // window.location.href = "https://www.google.com/";
+}
+
+function trocar(elemento){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+
+function voltar(elemento){
+    document.getElementById("mousemove2").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+
+}
+
+
+/*function soma(n1,n2){
     return n1 + n2;
 }
+
 var validar=0;
 
 function validadeIdade (idade){
@@ -16,6 +49,7 @@ function validadeIdade (idade){
 var idade= prompt("Qual sua idade")
 validadeIdade(idade)
 console.log(validadeIdade(idade))
+*/
 
 /*
 function setReplace(frase, nome, novo_nome){
